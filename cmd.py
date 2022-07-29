39f69059b7f971ebad470e6eab43a0202a8cdc05
init63 <shandy4wd@mail.ru>
2022-07-29 18:19:29 +0400

    UPD: refactory

--- cmd.py
+++ cmd.py
@@ -5,7 +5,7 @@ NameComponent = str
 
 
 @dataclass(slots=True, frozen=True)
-class CommandLineArguments:
+class CMDArguments:
     names: list[NameComponent]
     typescript: bool
     module: bool
@@ -15,9 +15,27 @@ class CommandLineArguments:
     less: bool
 
 
-def get_command_line_arguments() -> CommandLineArguments:
+def get_command_line_arguments() -> CMDArguments:
+    args = _create_argument_parser()
+    return CMDArguments(
+        names=args.names,
+        typescript=args.typescript,
+        module=args.module,
+        css=args.css,
+        scss=args.scss,
+        sass=args.sass,
+        less=args.less,
+    )
+
+
+def _create_argument_parser():
     parser = ArgumentParser(description="Bash script for creating React components.")
     group = parser.add_mutually_exclusive_group()
+    _configure_arguments(parser, group)
+    return parser.parse_args()
+
+
+def _configure_arguments(parser, group):
     parser.add_argument("names", nargs="+", help="Name[s] of React components.")
     parser.add_argument(
         "-t", "--typescript", help="Create a TypeScript component.", action="store_true"
@@ -29,14 +47,7 @@ def get_command_line_arguments() -> CommandLineArguments:
     group.add_argument("-s", "--scss", help="Use SCSS.", action="store_true")
     group.add_argument("-a", "--sass", help="Use SASS.", action="store_true")
     group.add_argument("-l", "--less", help="Use LESS.", action="store_true")
-    args = parser.parse_args()
 
-    return CommandLineArguments(
-        names=args.names,
-        typescript=args.typescript,
-        module=args.module,
-        css=args.css,
-        scss=args.scss,
-        sass=args.sass,
-        less=args.less,
-    )
+
+if __name__ == "__main__":
+    print(get_command_line_arguments())
