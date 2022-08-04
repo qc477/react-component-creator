328dc01c6889022d44888f738bf0f3eebdedbb92
init63 <shandy4wd@mail.ru>
2022-08-04 19:48:54 +0400

    UPD: move type CMDArgumets.

--- cmd.py
+++ cmd.py
@@ -1,25 +1,5 @@
-from dataclasses import dataclass
 from argparse import ArgumentParser
-
-NameComponent = str
-TypeComponent = str
-NumberOfSpaces = int
-
-
-@dataclass(slots=True, frozen=True)
-class CMDArguments:
-    names: list[NameComponent]
-    extension: TypeComponent
-    is_file: bool
-    is_folder: bool
-    is_module: bool
-    tab_width: NumberOfSpaces
-    is_semi: bool
-    is_single_quote: bool
-    is_css: bool
-    is_scss: bool
-    is_sass: bool
-    is_less: bool
+from types import CMDArguments
 
 
 def get_command_line_arguments() -> CMDArguments:
---
+++ types.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass
+
+NameComponent = str
+TypeComponent = str
+NumberOfSpaces = int
+
+
+@dataclass(slots=True, frozen=True)
+class CMDArguments:
+    names: list[NameComponent]
+    extension: TypeComponent
+    is_file: bool
+    is_folder: bool
+    is_module: bool
+    tab_width: NumberOfSpaces
+    is_semi: bool
+    is_single_quote: bool
+    is_css: bool
+    is_scss: bool
+    is_sass: bool
+    is_less: bool
